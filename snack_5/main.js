92ca4f7c3a6416016ec6efca3b9f3872599848b5
nicolaazonta <nicolazonta114@gmail.com>
2023-03-24 12:56:01 +0100

    i'm hungry

--- snack_5/main.js
+++ snack_5/main.js
@@ -2,8 +2,22 @@
 un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" */
 
+const array_5 = ['qui' , 'quo' , 'qua', 'conan', 'goku', 'banane'];
+const param_a = 2;
+const param_b = array_5.length;
+
+const newArray = [cacao(array_5 , param_a , param_b)];
 
 
-function cacao(param ) {
+function cacao(array_5 , param_a , param_b ) {
+    let intermediateValue = [];
+    for(let i=0, i< param_b - param_a, i++){
+        let valueX = param_b - 1;
+    }
+
+
+    intermediateValue.push(valueX);
     
-}
+
+    return  intermediateValue;
+} 
